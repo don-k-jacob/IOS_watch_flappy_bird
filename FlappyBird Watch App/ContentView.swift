7a5d035610fd6d89502f4d2f0f63db02b299df2a
don-k-jacob <donkjacob.business@gmail.com>
2023-01-24 11:13:35 +0530

    initial commit

--- FlappyBird Watch App/ContentView.swift
+++ FlappyBird Watch App/ContentView.swift
@@ -8,19 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var birdPosition = CGSize.zero
+    @State private var birdVelocity = CGSize.zero
+    @State private var pipeTop = CGFloat.random(in: 0...150)
+    @State private var pipeBottom = CGFloat.random(in: 0...150)
+    @State private var gameOver = false
+    @State private var score = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            if !gameOver {
+                Image("background")
+                    .resizable()
+                    .scaledToFill()
+                
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image("pipeTop")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: pipeTop)
+                        
+                        Image("pipeBottom")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: pipeBottom)
+                    }
+                    .offset(x: -100)
+                    .animation(.easeInOut(duration: 1.5))
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Image("bird")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 50)
+                            .offset(birdPosition)
+                            .animation(.spring())
+                    }
+                }
+                
+                Text("Score: \(score)")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .offset(x: -20, y: -20)
+            } else {
+                VStack {
+                    Text("Game Over!")
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                    
+                    Text("Your Score: \(score)")
+                        .font(.title)
+                        .foregroundColor(.red)
+                        .offset(x: -20, y: -20)
+                }
+            }
         }
-        .padding()
+        .onAppear(perform: startGame)
+        .onTapGesture {
+            if !self.gameOver {
+                withAnimation(.spring()) {
+                    self.birdVelocity.height = -10
+                }
+            } else {
+                self.startGame()
+            }
+        }
+        .animation(.easeInOut)
+    }
+    
+    func startGame() {
+        withAnimation(.default) {
+            self.birdPosition = CGSize.zero
+            self.birdVelocity = CGSize.zero
+            self.pipeTop = CGFloat.random(in: 0...150)
+            self.pipeBottom = CGFloat.random(in: 0...150)
+            self.gameOver = false
+            self.score = 0
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
