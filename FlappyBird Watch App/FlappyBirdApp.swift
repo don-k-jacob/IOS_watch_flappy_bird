cf9778b27992cdfa102c077c1706c591f683021c
don-k-jacob <donkjacob.business@gmail.com>
2023-01-24 09:58:01 +0530

    Initial Commit

---
+++ FlappyBird Watch App/FlappyBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlappyBirdApp.swift
+//  FlappyBird Watch App
+//
+//  Created by don kora jacob on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct FlappyBird_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
